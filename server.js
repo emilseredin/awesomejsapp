c6e888df0d6b9e3a02bdba648a2213b700769b39
Emil Seredin <emils.seredins@gmail.com>
2020-05-21 23:11:35 +0300

    add preprocessed html and css files, modify server response logic

--- server.js
+++ server.js
@@ -9,16 +9,25 @@ const server = http.createServer((req, res) => {
     let data = "";
     if (req.url == "/") {
         data = fs.readFileSync("index.html", "utf-8");
-    } else if (req.url == "/style.css") {
-        data = fs.readFileSync("style.css", "utf-8");
+        res.end(data);
+    } else if (req.url == "/css/main.css") {
+        data = fs.readFileSync("./css/main.css", "utf-8");
+        res.end(data);
+    } else if (req.url == "/images/borlaug.jpg") {
+        fs.readFile("./images/borlaug.jpg", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            res.writeHead(200, {"Content-Type": "image/jpeg"})
+            res.end(data, "base64");
+        });
     }
-    res.end(data);
 });
 
 
 //heroku doesn't know anything about port 3000
 //use process.env.PORT variable to get access to the port that is specified by heroku
 //e.g. PORT=1234 node server.js
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 3000);
 // script is running now, and waiting for requests on port 3000
 console.log("Server has started!");
